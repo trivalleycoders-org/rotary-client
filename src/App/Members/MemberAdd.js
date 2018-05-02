fdc020b4cbcf9493561470fbaa445383583ffdf6
klequis <klequis@gmail.com>
2018-05-02 16:33:22 -0700

    wip: AddMember

--- src/App/Members/MemberAdd.js
+++ src/App/Members/MemberAdd.js
@@ -1,29 +1,85 @@
 import React, { Component } from 'react'
+import shortid from 'shortid'
 import Phone from './Phone'
+import { clone } from 'ramda'
+import { yellow } from 'logger'
 
 class MemberAdd extends Component {
   constructor(props) {
     super(props)
+    const id = shortid.generate()
     this.state = {
       firstName: '',
       lastName: '',
       exempt: false,
+      isGoing: false,
       avoidRoles: [],
-      comments: [],
-      phoneType: '',
-      phoneNumber: '',
+      comment: '',
+      phones: [
+        { id: id, type: 'mobile', number: '' },
+      ],
       email: '',
     }
   }
+  handleTypeChange = (id, type) => {
+    yellow(`id=${id}, type=${type}`)
+    const { phones } = this.state
+    let newPhones = clone(phones)
+    const index = newPhones.findIndex((p, index) => p.id === id)
+    newPhones[index].type = type
+    this.setState({
+      phones: newPhones
+    })
+  }
+  handleNumberChange = (id, number) => {
+    yellow(`id=${id}, type=${number}`)
+    const { phones } = this.state
+    let newPhones = clone(phones)
+    const index = newPhones.findIndex((p, index) => p.id === id)
+    newPhones[index].number = number
+    this.setState({
+      phones: newPhones
+    })
+  }
+
+  handleInputChange = (e) => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleAddPhoneClick = () => {
+    yellow('addPhoneClick')
+    const { phones } = this.state
+    let newPhones = clone(phones)
+    newPhones.push({ id: shortid.generate(), type: 'mobile', number: '' })
+    this.setState({
+      phones: newPhones,
+    })
+  }
+  renderPhones = () => {
+    const phones = this.state.phones
+    return phones.map((p, index) => {
+      return (<Phone key={index} id={p.id} type={p.type} number={p.number} handleTypeChange={this.handleTypeChange} handleNumberChange={this.handleNumberChange} />)
+    })
+  }
   render() {
+
     return (
       <div>
         <h1>Add Member</h1>
-        <input type='text' name='firstName' placeholder='first name' />
-        <input type='text' name='lastName' placeholder='last name' />
-        <div>roles go here as a component not yet built</div>
-        <textarea rows='4' cols='50' name='comment' placeholder='comments' /><br/>
-        <Phone />
+        <input type='text' name='firstName' placeholder='first name' onChange={(e) => this.handleInputChange(e)} />
+        <input type='text' name='lastName' placeholder='last name' onChange={(e) => this.handleInputChange(e)} />
+        <h2>Roles</h2>
+        <label htmlFor='exempt'>Exempt </label><input name='exempt' type='checkbox'  checked={this.state.exempt} onChange={this.handleInputChange} />
+        <h2>Comment</h2>
+        <textarea rows='4' cols='50' name='comment' placeholder='comments' onChange={this.handleInputChange} /><br/>
+        <h2>Phone</h2>
+        <button onClick={this.handleAddPhoneClick}>Add Phone</button>
+        {this.renderPhones()}
 
       </div>
 
--- src/App/Members/Phone.js
+++ src/App/Members/Phone.js
@@ -1,58 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { yellow } from 'logger'
 
-class Phone extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      phoneNumbers: [],
-      phone1Value: {
-        type: 'mobile',
-        value: '',
-      },
-      phone2Value: '',
-    }
-  }
-
-  updatePhoneNumbers = () => {
-    let numbers = this.state.phoneNumbers
-
-  }
-  handleSelectChange = (e) => {
-    yellow('handleSelectChange')
-
-    const key = `${e.target.name}Value`
-    const value = e.target.value
-    yellow(`${key}:${value}`)
-    this.setState({
-      [key]: value,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <div>
-          <label htmlFor='phone1'>Phone 1: </label>
-          <select value={this.state.phone1Value}name='phone1' onChange={(e) => this.handleSelectChange(e)}>
-            <option value='mobile'>Mobile</option>
-            <option value='home' >Home</option>
-            <option value='work'>Work</option>
-          </select>
-          <input type='text' name='phone1' placeholder='phone number' />
-        </div>
-        <div>
-          <label htmlFor='phone2'>Phone 2: </label>
-          <select value={this.state.phone2Value} name='phone2' onChange={(e) => this.handleSelectChange(e)}>
-            <option value=''>select</option>
-            <option value='mobile' selected>Mobile</option>
-            <option value='home' >Home</option>
-            <option value='work'>Work</option>
-          </select>
-          <input type='text' name='phone2' placeholder='phone number' />
-        </div>
-      </div>
-    )
-  }
+const Phone = ({ id, type, number, handleTypeChange, handleNumberChange }) => {
+  return (
+    <div>
+      <select value={type} name='phone2' onChange={(e) => handleTypeChange(id, e.target.value)}>
+        <option value='mobile' selected>Mobile</option>
+        <option value='home' >Home</option>
+        <option value='work'>Work</option>
+      </select>
+      <input type='text' name='phone2' placeholder='phone number' value={number} onChange={(e) => handleNumberChange(id, e.target.value)}/> id={id}
+    </div>
+  )
 }
 export default Phone

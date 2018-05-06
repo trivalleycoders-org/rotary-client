2f60c2a5f2807e9955aeb172e29fdd98b16006eb
klequis <klequis@gmail.com>
2018-05-05 20:42:25 -0700

    removed h1 etc test code

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,9 +4,7 @@ import Members from './Members'
 import { withStyles } from 'material-ui/styles'
 import withRoot from '../withRoot'
 import Typography from 'material-ui/Typography'
-import { withTheme } from 'material-ui/styles';
-import H1 from 'elements/H1'
-import H2 from 'elements/H2'
+
 const App = (props) => {
   return (
     <div>
@@ -16,5 +14,3 @@ const App = (props) => {
 }
 
 export default withRoot(App)
-// import { withTheme } from 'material-ui/styles'
-// export default withTheme()(MyComponent);
